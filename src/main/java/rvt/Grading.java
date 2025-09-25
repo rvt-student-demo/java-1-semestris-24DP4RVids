ee5b51c0b877a0216897914cf5452ff64208cbea
24DP4RVids <A240495RV@rvt.lv>
2025-09-25 00:28:38 -0700

    Grading Vids

--- src/main/java/rvt/Grading.java
+++ src/main/java/rvt/Grading.java
@@ -1,42 +1,28 @@
 import java.util.Scanner;
 
-public class average {
-    public static void main(String[]args){
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Ievadiet Atzīmi: ")
-    Double atzime = scanner.nextDouble();
-    if (atzime < 0){
-        System.out.println("impossible")
-
-    }
-    if (atzime <= 49){
-        System.out.println("failed")
-                
-    }
-    if (atzime <= 59){
-        System.out.println("1")
-                
-    }
-    if (atzime <= 69){
-        System.out.println("2")
-                
-    }
-    if (atzime <= 79){
-        System.out.println("3")
-                
-    }
-    if (atzime <= 89){
-        System.out.println("4")
-                
-    }
-    if (atzime <= 99){
-        System.out.println("5")
-                
-    }
-    if (atzime >= 100){
-        System.out.println("Incredible")
-                
-    }
-    
+public class Average {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Ievadiet Atzīmi: ");
+        double atzime = scanner.nextDouble();
+        
+        if (atzime < 0) {
+            System.out.println("Impossible");
+        } else if (atzime <= 49) {
+            System.out.println("Failed");
+        } else if (atzime <= 59) {
+            System.out.println("1");
+        } else if (atzime <= 69) {
+            System.out.println("2");
+        } else if (atzime <= 79) {
+            System.out.println("3");
+        } else if (atzime <= 89) {
+            System.out.println("4");
+        } else if (atzime <= 99) {
+            System.out.println("5");
+        } else if (atzime >= 100) {
+            System.out.println("Incredible");
+        }
     }
 }
