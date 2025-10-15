9f66ea4a7ab6488a3166967af0da1eb209f8c8f6
24DP4RVids <A240495RV@rvt.lv>
2025-10-15 07:24:56 +0000

    masivi trennins

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,13 +1,24 @@
 package rvt;
 
+import java.util.Scanner;;
+
 public class App {
     public static void main(String[] args) {
-        // Your code goes here...
+        // type[] arrayname = new type[lenght];
+        int lenght = 10; // input
+        int[] skaitli = new int[lenght];
+        // [3, 11, 7, 0, 0]
+        skaitli[0] = 3;
+        skaitli[1] = 11;
+        skaitli[2] = 7;
+
+        // String[] strArr = new String[4];
+
     Scanner scanner = new Scanner(System.in);
     System.out.println("Give speed: ");
-    int speed = scanner.nextline();
-    if (speed => 120){
-        System.out.println("Speeding ticket")
+    int speed = scanner.nextInt();
+    if (speed >= 120){
+        System.out.println("Speeding ticket");
     }
     }
 }
