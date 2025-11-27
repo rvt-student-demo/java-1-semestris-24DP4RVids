3695c0954cbc66a6c36dfac52435bb973a5d2e57
24DP4RVids <A240495RV@rvt.lv>
2025-11-27 07:09:00 +0000

    Random kods kur var testēt

---
+++ src/main/java/rvt/Trennins.java
@@ -0,0 +1,24 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Trennins {
+    public static void main(String[] args) {
+        // type[] arrayname = new type[lenght];
+        int lenght = 10; // input
+        int[] skaitli = new int[lenght];
+        // [3, 11, 7, 0, 0]
+        skaitli[0] = 3;
+        skaitli[1] = 11;
+        skaitli[2] = 7;
+
+        // String[] strArr = new String[4];
+        System.out.println(skaitli[0] + skaitli[1] - skaitli[2]);
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Give speed: ");
+    int speed = scanner.nextInt();
+    if (speed >= 120){
+        System.out.println("Speeding ticket");
+    }
+    }
+}
